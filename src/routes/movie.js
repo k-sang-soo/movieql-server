9026e3d36fa5a916912823b4b4e79d6723a88f0f
k-sang-soo <wsckm1@google.com>
2023-06-03 18:41:52 +0900

    라우터 세팅

---
+++ src/routes/movie.js
@@ -0,0 +1,5 @@
+function Movie() {
+    return <div>123123123</div>;
+}
+
+export default Movie;
---
+++ src/routes/movies.js
@@ -0,0 +1,5 @@
+function Movies() {
+    return <div>asdasdasdasd</div>;
+}
+
+export default Movies;
