6c17c2356f07070d1b9b97644c5af6137e0b5b49
k-sang-soo <wsckm1@google.com>
2023-06-04 13:16:48 +0900

    apollo client hook으로 데이터 뿌려보기

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,5 +1,30 @@
+import { gql, useApolloClient } from '@apollo/client';
+import { useEffect, useState } from 'react';
+
 function Movies() {
-    return <div>asdasdasdasd</div>;
+    const [movies, setMovies] = useState([]);
+    const client = useApolloClient();
+    useEffect(() => {
+        client
+            .query({
+                query: gql`
+                    {
+                        allMovies {
+                            id
+                            title
+                        }
+                    }
+                `,
+            })
+            .then((res) => setMovies(res.data.allMovies));
+    }, [client]);
+
+    return (
+        <ul>
+            {movies.length > 0 &&
+                movies.map((movie) => <li key={movie.id}>{movie.title}</li>)}
+        </ul>
+    );
 }
 
 export default Movies;
